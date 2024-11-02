8d8d329cc9faba2e28a2f46623115c945f1b2ec8
Abhay7286 <abhaymishra7286@gmail.com>
2024-11-02 17:30:09 +0530

    2 commit
    added  (i+1) in setVal function

--- Array_ADT.c
+++ Array_ADT.c
@@ -1,46 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct myArray
-{
+struct myArray {
     int total_size;
     int used_size;
     int *ptr;
 };
 
-void createArray(struct myArray*a,int tsize,int usize)
-{
+void createArray(struct myArray *a, int tsize, int usize) {
     a->total_size = tsize;
     a->used_size = usize;
-    a->ptr = (int*)malloc(tsize*sizeof(int));
-
+    a->ptr = (int *)malloc(tsize * sizeof(int));
 }
 
-void setVal(struct myArray*a)
-{
+void setVal(struct myArray *a) {
     int n;
-    for (int i = 0; i < a->used_size; i++)
-    {
-        printf("enter the element: %d\n");
-        scanf("%d",&n);
+    for (int i = 0; i < a->used_size; i++) {
+        printf("Enter the element %d:\n", i + 1);
+        scanf("%d", &n);
         (a->ptr)[i] = n;
     }
-    
 }
 
-void show(struct myArray*a)
-{
-    for (int i = 0; i < a->used_size; i++)
-    {
-        printf("%d\n",(a->ptr)[i]);
+void show(struct myArray *a) {
+    for (int i = 0; i < a->used_size; i++) {
+        printf("%d\n", (a->ptr)[i]);
     }
-    
 }
 
-
-int main(){
+int main() {
     struct myArray marks;
-    createArray(&marks,5,2);
+    createArray(&marks, 5, 2);
     setVal(&marks);
     show(&marks);
     return 0;
